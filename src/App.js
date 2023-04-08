327d43b7f3d91f6b8ad62f542c966e58f242eee8
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-08 14:16:40 +0700

    feat: create header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import './styles/App.css';
 import React from 'react';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="dropdown">
-      <label tabIndex={0} className="btn m-1">
-        Click
-      </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a href="/">Item 1</a>
-        </li>
-        <li>
-          <a href="/">Item 2</a>
-        </li>
-      </ul>
+    <div className="min-h-screen">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
