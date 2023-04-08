d3ffdb58029aac762c368a5062c65ed019815b01
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-08 17:50:25 +0700

    feat: add pagination component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css';
 import React from 'react';
 import Header from './components/Header';
+import Pagination from './components/Pagination';
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <Header />
+      <Pagination />
     </div>
   );
 };
