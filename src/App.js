005e40d596ba061a2956cf0b543ccb83eefc569b
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-10 23:07:38 +0700

    feat: add customers table component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './styles/App.css';
 import React from 'react';
+import './styles/App.css';
 import Header from './components/Header';
 import Pagination from './components/Pagination';
 import AnalyticsInfo from './components/AnalyticsInfo';
 import TopMenu from './components/TopMenu';
 import Hero from './components/Hero';
 import SideBar from './components/SideBar';
+import CustomersTable from './components/CustomersTable';
 
 const App = () => {
   return (
@@ -14,11 +15,12 @@ const App = () => {
       <div className="w-full pl-1 xl:pl-4">
         <Header />
         <div className="flex flex-col justify-between mt-4 lg:flex-row">
-          <div className="mr-3 lg:w-[80%] 2xl:w-auto">
+          <div className="mr-3 lg:w-[80%] 2xl:w-auto mb-6">
             <Hero />
+            <CustomersTable />
             <Pagination />
           </div>
-          <div className="flex justify-evenly lg:block">
+          <div className="flex flex-col items-center justify-around gap-0 mb-3 xs:gap-x-1 lg:gap-x-0 xs:flex-row lg:block lg:mb-0">
             <AnalyticsInfo />
             <TopMenu />
           </div>
