bfbe5827fccdf5182bd86df2b40ef44f0691857f
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-10 23:08:44 +0700

    feat: add sorting option data

---
+++ src/assets/data/sortingOption.js
@@ -0,0 +1,33 @@
+export const nameSortOptions = [
+  { label: 'Customer Name', value: 'customer name', disabled: true },
+  { label: 'Sort By Name A-Z', value: 'ascending' },
+  { label: 'Sort By Name Z-A', value: 'descending' },
+];
+
+export const levelSortOption = [
+  { label: 'Level', value: 'level', disabled: true },
+  { label: 'Warga', value: 'warga' },
+  { label: 'Juragan', value: 'juragan' },
+  { label: 'Sultan', value: 'sultan' },
+  { label: 'Kolongmerat', value: 'kolongmerat' },
+];
+
+export const favoriteMenuSortOption = [
+  { label: 'Favorite Menu', value: 'favorite menu', disabled: true },
+  { label: 'Chicken & Ribs Combo', value: 'chicken & ribs combo' },
+  { label: 'Fried Chicken Dinne', value: 'Fried Chicken Dinne' },
+  { label: 'Surf & Turf Gift Basket', value: 'surf & turf gift basket' },
+  { label: 'Dark & Stormy', value: 'dark & stormy' },
+  { label: 'Shaking Beef Tri-Tip', value: 'shaking beef tri-tip' },
+  { label: 'BBQ Rib Dinner', value: 'bbq rib dinner' },
+];
+
+export const totalTransactionSortOption = [
+  { label: 'Total Transaction', value: 'Total Transaction', disabled: true },
+  { label: 'Less than 100,000', value: '<100,000' },
+  { label: '100,000 - 200,000', value: '100,000-200,000' },
+  { label: '200,000 - 300,000', value: '200,000-300,000' },
+  { label: '300,000 - 400,000', value: '300,000-400,000' },
+  { label: '400,000 - 500,000', value: '400,000-500,000' },
+  { label: 'More than 500,000', value: '>500,000' },
+];
