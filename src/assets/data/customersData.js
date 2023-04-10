1a6a3637910a988f3329f0150dec575ce4680cb1
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-10 23:07:58 +0700

    feat: add customers data

---
+++ src/assets/data/customersData.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    customerName: 'Odis Rhinehart',
+    level: 'Warga',
+    favoriteMenu: 'Chicken & Ribs Combo',
+    totalTransaction: '194,700',
+  },
+  {
+    customerName: 'Kris Roher',
+    level: 'Warga',
+    favoriteMenu: 'Surf & Turf Gift Basket',
+    totalTransaction: 'IDR 631,200',
+  },
+  {
+    customerName: 'Serenity Fisher',
+    level: 'Juragan',
+    favoriteMenu: 'Fried Chicken Dinne',
+    totalTransaction: 'IDR 1,040.920',
+  },
+  {
+    customerName: 'Brooklyn Warren',
+    level: 'Sultan',
+    favoriteMenu: 'Surf & Turf Gift Basket',
+    totalTransaction: 'IDR 730,500',
+  },
+  {
+    customerName: 'Franco Delort',
+    level: 'Juragan',
+    favoriteMenu: 'Chicken & Ribs Combo',
+    totalTransaction: 'IDR 96,000',
+  },
+  {
+    customerName: 'Saul Geoghegan',
+    level: 'Juragan',
+    favoriteMenu: 'Surf & Turf Gift Basket',
+    totalTransaction: 'IDR 256,000',
+  },
+  {
+    customerName: 'Alfredo Vetrovs',
+    level: 'Juragan',
+    favoriteMenu: 'Dark & Stormy',
+    totalTransaction: 'IDR 590,080',
+  },
+  {
+    customerName: 'Cristofer Vetrovs',
+    level: 'Konglomerat',
+    favoriteMenu: 'Shaking Beef Tri-Tip',
+    totalTransaction: 'IDR 782,600',
+  },
+  {
+    customerName: 'Calvin Steward',
+    level: 'Konglomerat',
+    favoriteMenu: 'BBQ Rib Dinner',
+    totalTransaction: 'IDR 467,500',
+  },
+  {
+    customerName: 'John Rambo',
+    level: 'Konglomerat',
+    favoriteMenu: 'BBQ Rib Dinner',
+    totalTransaction: 'IDR 467,500',
+  },
+];
