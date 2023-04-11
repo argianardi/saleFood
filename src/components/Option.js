3fcd2c03eed1b7c922de35e2308e74a86c3aef8d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-11 08:52:41 +0700

    style: fix font style

--- src/components/Option.js
+++ src/components/Option.js
@@ -40,7 +40,7 @@ const Option = ({ options }) => {
                   selected.value === option.value
                     ? 'bg-gray-200 text-gray-900'
                     : 'text-gray-700 hover:bg-gray-100'
-                } block w-full text-left px-4 py-2 text-sm`}
+                } block w-full text-left px-4 py-2 text-xs font-quicksand`}
                 onClick={() => handleSelect(option)}
               >
                 {option.label}
