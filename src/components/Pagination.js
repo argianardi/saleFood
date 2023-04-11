4ce9050171cff175f39c1be1b7ed77e78a1dd64e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-04-11 15:02:06 +0700

    feature(pagination): implement pagination navigation

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,50 +1,74 @@
-import React from 'react';
-import { HiArrowLongRight } from 'react-icons/hi2';
+import React, { useState } from 'react';
+import { HiArrowLongLeft, HiArrowLongRight } from 'react-icons/hi2';
 
-const Pagination = () => {
+const Pagination = ({ totalPages }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleClickNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handleClickPrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+    const startIndex = currentPage - 1;
+    const endIndex = startIndex + 3;
+
+    return pageNumbers.slice(startIndex, endIndex).map((pageNumber) => (
+      <button
+        key={pageNumber}
+        type="button"
+        className={`px-4 py-2 text-sm font-bold duration-300 transform rounded-md font-quicksand ${
+          pageNumber === currentPage
+            ? 'bg-white shadow-sm'
+            : 'text-gray-medium hover:bg-white hover:shadow-sm'
+        }`}
+        onClick={() => setCurrentPage(pageNumber)}
+      >
+        {pageNumber}
+      </button>
+    ));
+  };
+
   return (
-    <div className="flex flex-col items-start justify-between px-1 py-2 mt-8 rounded-lg lg:mt-16 xs:px-3 lg:items-center lg:flex-row bg-white-smoke">
+    <div className="flex flex-col items-start justify-between px-1 py-2 mt-8 rounded-lg lg:mt-16 xs:px-1 lg:items-center lg:flex-row bg-white-smoke">
       <p className="text-base font-semibold font-quicksand text-gray-light">
         Showing 10 Data Customers
       </p>
       <div className="flex items-center h-8 mx-auto mt-4 lg:mx-0 lg:mt-0">
-        <button
-          type="button"
-          className="px-4 py-2 text-sm font-bold bg-white rounded-md shadow-sm font-quicksand"
-        >
-          1
-        </button>
-        <button
-          type="button"
-          className="text-sm btn btn-ghost font-quicksand text-gray-medium"
-        >
-          2
-        </button>
-        <button
-          type="button"
-          className="text-sm btn btn-ghost font-quicksand text-gray-medium"
-        >
-          3
-        </button>
-        <button
-          type="button"
-          className="text-sm btn btn-ghost font-quicksand text-gray-medium"
-        >
-          ...
-        </button>
-        <button
-          type="button"
-          className="text-sm btn btn-ghost font-quicksand text-gray-medium"
-        >
-          38
-        </button>
-        <button
-          type="button"
-          className="flex items-center text-sm btn btn-ghost font-quicksand text-gray-medium"
-        >
-          <p className="mr-0 xs:mr-2">Next</p>
-          <HiArrowLongRight size={25} className="text-sm xs:text-3xl" />
-        </button>
+        {currentPage > 1 && (
+          <button
+            type="button"
+            className="flex items-center px-1 min-[430]:px-4 py-2 text-sm font-bold duration-300 transform rounded-md font-quicksand text-gray-medium hover:bg-white hover:shadow-sm"
+            onClick={handleClickPrev}
+          >
+            <HiArrowLongLeft size={25} className="text-sm xs:text-3xl" />
+            <span className="hidden ml-0 xs:block xs:ml-2">Prev</span>
+          </button>
+        )}
+        {renderPagination()}
+        {currentPage < totalPages - 2 && (
+          <>
+            <span className="px-4 py-2 text-sm font-bold duration-300 transform rounded-md font-quicksand text-gray-medium">
+              ...
+            </span>
+            <button
+              type="button"
+              className="flex items-center px-1 min-[430]:px-4 py-2 text-sm font-bold duration-300 transform rounded-md font-quicksand text-gray-medium hover:bg-white hover:shadow-sm"
+              onClick={handleClickNext}
+            >
+              <span className="hidden mr-0 xs:block xs:mr-2">Next</span>
+              <HiArrowLongRight size={25} className="text-sm xs:text-3xl" />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
